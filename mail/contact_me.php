db7f619ecef4c096644336fd620688911bd1cb02
m3at <paul.willot@laposte.net>
2015-09-01 17:41:47 +0900

    errata

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,24 +1,11 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
+$to      = 'kiwi.pwillot@gmail.com';
+$subject = 'the subject';
+$message = 'hello';
+$headers = 'From: kiwi.pwillot@gmail.com' . "\r\n" .
+    'Reply-To: kiwi.pwillot@gmail.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$message = $_POST['message'];
-
-// Create the email and send the message
-$to = 'paul.willot.2@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "Hey future myself, you have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "paul.willot.2@gmail.com\n"; //From:   This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
+mail($to, $subject, $message, $headers);
 return true;
 ?>
